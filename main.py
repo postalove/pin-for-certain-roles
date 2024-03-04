ff4ec38cb65ea4a2f5f55ad746e714d909107c3a
postalove <55823600+postalove@users.noreply.github.com>
2024-03-04 19:47:35 +0800

    .

--- main.py
+++ main.py
@@ -19,39 +19,31 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 import interactions
 # Use the following method to import the internal module in the current same directory
-from . import internal_t
+
 
 '''
 Replace the ModuleName with any name you'd like
 '''
-class ModuleName(interactions.Extension):
+class PinMessage(interactions.Extension):
     module_base: interactions.SlashCommand = interactions.SlashCommand(
-        name="replace_your_command_base_here",
-        description="Replace here for the base command descriptions"
+        name="pin",
+        description="标注"
     )
-    module_group: interactions.SlashCommand = module_base.group(
-        name="replace_your_command_group_here",
-        description="Replace here for the group command descriptions"
-    )
+    
 
-    @module_group.subcommand("ping", sub_cmd_description="Replace the description of this command")
+    @module_base.subcommand("message", sub_cmd_description="Replace the description of this command")
     @interactions.slash_option(
-        name = "option_name",
+        name = "消息链接",
         description = "Option description",
         required = True,
         opt_type = interactions.OptionType.STRING
     )
-    async def module_group_ping(self, ctx: interactions.SlashContext, option_name: str):
-        await ctx.send(f"Pong {option_name}!")
-        internal_t.internal_t_testfunc()
-
-    @module_base.subcommand("pong", sub_cmd_description="Replace the description of this command")
-    @interactions.slash_option(
-        name = "option_name",
-        description = "Option description",
-        required = True,
-        opt_type = interactions.OptionType.STRING
-    )
-    async def module_group_pong(self, ctx: interactions.SlashContext, option_name: str):
-        await ctx.send(f"Pong {option_name}!")
-        internal_t.internal_t_testfunc()
+    async def pin_mess(self, ctx: interactions.SlashContext, message_url: str):
+        pin_role=ctx.guild.get_role(1214173923206234142)
+        if ctx.author.has_role(pin_role):
+            message_id=message_url.rsplit('/', 1)[-1]
+            message=ctx.channel.get_message(message_id=message_id)
+            await message.pin()
+            await ctx.send(content="Message Pinned!",ephemeral=True)
+        
+    
